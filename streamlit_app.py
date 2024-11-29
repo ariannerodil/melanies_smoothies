f7a76fb4274422aea054ffcefcf8f2c56abe9e84
ariannerodil <arianne.rodil@accenture.com>
2024-11-29 11:51:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,11 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!'+ name_an_order, icon="✅")
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.jason(), use_container_width=True)
+
+if ingredients_list:
+    ingredients_string= ''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
